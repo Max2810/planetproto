16d3576c9f990695f5b408d68e733526dd79585a
Max2810 <maxime.fery@outlook.fr>
2020-02-13 10:18:07 +0100

    exo1 done

--- 010.js
+++ 010.js
@@ -1,11 +1,16 @@
 // -> Créez un objet appelé `robot` à l’aide d’un objet littéral
 // -> `robot` devrait avoir une propriété `smart` qui vaut `true`
-__
+
+
+const robot = {
+	smart: true
+};
+
 
 // -> Affirmez le résultat de `robot.smart`
-claim(robot.smart, __);
+claim(robot.smart, true);
 
-// -----------------------------------------------------
+
 // Export CommonJS pour le vérificateur, n’y touchez pas
 module.exports = {
 	robot: robot
